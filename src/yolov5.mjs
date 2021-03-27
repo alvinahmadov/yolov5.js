d73d427632679624fa7308b53e816218996ceb40
Alvin Ahmadov <alvin.dev.ahmadov@gmail.com>
2021-03-27 05:48:10 +0400

    Update for windows as a submodule

--- src/yolov5.mjs
+++ src/yolov5.mjs
@@ -1,8 +1,8 @@
-import api     from "@pipcook/boa";
-import path    from "path";
-import * as fs from "fs";
-
-export const __dirname = path.resolve(path.dirname(''));
+import path 			from "path";
+import * as fs 			from "fs";
+import { spawn } 		from "child_process"
+import { OS, PACKAGES } from "./config.mjs";
+import { getAbsPath } 	from "./utils.mjs";
 
 export class YOLOv5 {
 	/**
@@ -13,21 +13,27 @@ export class YOLOv5 {
 	 * @param {boolean} agnosticNMS: Class-agnostic NMS
 	 * @param {boolean} augment: Augmented inference
 	 * */
-	constructor(model,
-	            device = 'cpu',
-	            iouThres = 0.4,
-	            confThres = 0.5,
-	            agnosticNMS = false,
-	            augment = false) {
+	constructor(
+		model,
+		device = 'cpu',
+		iouThres = 0.4,
+		confThres = 0.5,
+		agnosticNMS = false,
+		augment = false
+	) {
 		this.model = model;
 		this.iouThres = iouThres;
 		this.confThres = confThres;
 		this.agnosticNMS = agnosticNMS;
 		this.augment = augment;
 		this.device = device;
-		this.yolo = api.import('yolov5');
+		this.workdir = path.resolve('node_modules', 'yolov5', PACKAGES['yolov5']);
+		this.pyexe = (OS === "Win32")
+			? path.resolve(this.workdir, 'venv', 'Scripts', 'python.exe')
+			: path.resolve(this.workdir, 'venv', 'bin', 'python');
+
 	}
-	
+
 	/**
 	 * @param {string} image: Image to detect
 	 * @param {number} size: Image resize
@@ -36,23 +42,43 @@ export class YOLOv5 {
 	detect(image, size = 640, saveDir = 'detections') {
 		if (!fs.existsSync(image))
 			throw URIError(`File ${image} doesn't exist!`);
-		let out = image.slice(image.lastIndexOf('/') + 1, image.lastIndexOf('.'));
-		this.yolo.detect(
-			api.kwargs({
-				           source:       image,
-				           img_size:     size,
-				           weights:      this.model,
-				           conf_thres:   this.confThres,
-				           iou_thres:    this.iouThres,
-				           augment:      this.augment,
-				           device:       this.device,
-				           view_img:     false,
-				           agnostic_nms: this.agnosticNMS,
-				           project:      path.resolve(__dirname, saveDir),
-				           name:         out,
-				           save_txt:     true,
-				           save_conf:    true
-			           }));
+
+		let out = image.slice(image.lastIndexOf(path.sep) + 1, image.lastIndexOf('.'));
+		let kwargs = [path.resolve(this.workdir, 'detect.py')];
+		kwargs.push(`--weights=${this.model}`);
+		kwargs.push(`--source=${image}`);
+		kwargs.push(`--conf-thres=${this.confThres}`);
+		kwargs.push(`--iou-thres=${this.iouThres}`);
+		kwargs.push(`--device=${this.device}`);
+		kwargs.push(`--project=${getAbsPath(saveDir)}`);
+		kwargs.push(`--name=${out}`);
+		kwargs.push('--save-txt');
+		kwargs.push('--save-conf');
+
+		if (this.agnosticNMS)
+			kwargs.push('--agnostic_nms');
+		if (this.augment)
+			kwargs.push('--augment');
+
+		let res = spawn(this.pyexe, kwargs);
+
+		res.stdout.on('data', (data) => {
+			console.log(data.toString());
+		});
+
+		res.stderr.on('data', (data) => {
+			console.error(data.toString());
+		})
+
+		res.on('error', function (e) {
+			console.error(e)
+		})
+
+		res.on('close', function (code) {
+			if (code === 0)
+				console.log("Exit code: Success");
+			else
+				console.log("Exit code: ", code);
+		})
 	}
 }
-
